3ad6290c3c8bfa606d88e41b989ebbad98b4fbe2
lucianoalessi9 <lucianoalessi9@hotmail.com>
2023-06-08 16:04:31 -0300

    all

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Tarea from './Componentes/Tarea';
 
 function App() {
   return (
-    <div className="App">
-    
+    <div className='aplicacion-tareas'>
+      <div className='tarea-lista-principal'>
+        <h1>Mis tareas</h1>
+        <Tarea texto='aprender react'/>
+        <Tarea />
+        <Tarea />
+        
+
+      </div>
+
     </div>
   );
 }
