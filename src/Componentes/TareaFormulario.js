e646f61955d64533e3ae2df062942cc3b9655455
lucianoalessi9 <lucianoalessi9@hotmail.com>
2023-06-08 19:38:18 -0300

    submit

--- src/Componentes/TareaFormulario.js
+++ src/Componentes/TareaFormulario.js
@@ -1,15 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import '../Hojas-de-estilo/TareaFormulario.css'
 
 function TareaFormulario(props) {
+
+    const [input, setInput] = useState('')
+
+
+    const manejarCambio = e => {
+        setInput(e.target.value)
+    }
+    
+    
+    const manejarEnvio = e => {
+      e.preventDefault();
+      
+      const nuevaTarea = {
+        id: '123',
+        texto: input,
+        completada: false
+    }
+}
+
+
     return(
-        <form className="tarea-formulario">
+        <form className="tarea-formulario"
+        onSubmit={manejarEnvio}>
             <input
             className="tarea-input"
             type="text"
             placeholder="Escribe una Tarea"
-            name="texto"/>
-            <button className="tarea-boton">
+            name="texto"
+            onChange={manejarCambio}/>
+            <button className="tarea-boton"
+            type="submit">
                 Agregar Tarea
             </button>
         </form>
