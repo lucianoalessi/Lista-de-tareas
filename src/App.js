5d38aa5a56091d3f232fb74c52316406c71ddbc8
lucianoalessi9 <lucianoalessi9@hotmail.com>
2023-07-15 16:17:58 -0300

    refactor(app.js): Improved code structure

--- src/App.js
+++ src/App.js
@@ -4,19 +4,15 @@ import ListaDeTareas from './Componentes/ListaDeTareas';
 function App() {
   return (
     <div className='aplicacion-tareas'>
-
       <div className='tarea-lista-principal'>
         <h1>Mis tareas</h1>
         <ListaDeTareas/>
       </div>
-
       <div className='author'>
         Designed and Coded by
         <br/>
         <a href='https://www.linkedin.com/in/lucianoalessi/' target='_blank'>Luciano A. Alessi</a>
-      </div>
-
-        
+      </div> 
     </div>
   );
 }
